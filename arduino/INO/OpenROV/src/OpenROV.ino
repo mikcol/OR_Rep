2d3740096df2635e7d064bb4216045b397193929
Mikcol <mikkel.cornelius@gmail.com>
2014-05-20 19:42:55 +0200

    Updated OpenROV.ino with functionality

--- arduino/INO/OpenROV/src/OpenROV.ino
+++ arduino/INO/OpenROV/src/OpenROV.ino
@@ -1,42 +1,58 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <arduino_msgs/Commands.h>
+#include <arduino_msgs/Thruster.h>
+#include <arduino_msgs/AuxSense.h>
 #include <arduino_msgs/NavData.h>
 #include <Wire.h>
 #include <MPU6050.h>
 #include "Thruster.h"
+#include "MS5803_14BA.h"
+#include "ControllerBoard.h"
+#include "CameraMount.h"
+#include "Light.h"
+#include "Laser.h"
 #include <utility/twi.h>
 ros::NodeHandle nh;
 
+ControllerBoard ctrl_board;
 MPU6050 accelgyro;
+MS5803_14BA depth_sense;
+CameraMount camera;
+Thruster thrust;
+Light LED;
+Laser LaserLED;
+
 int16_t ax,ay,az;
 int16_t gx,gy,gz;
-Thruster thrust;
 arduino_msgs::NavData navdat;
-arduino_msgs::Commands cmd;
+arduino_msgs::AuxSense auxdat;
 bool run = 1;
-bool status = 0;
+uint8_t update_nav = 0;
+uint8_t update_aux = 0;
 
-void timer1_init(){
+void timer3_init(){
 	//Timer Interrupt
-	TCCR1A = 0;
-	TCCR1B = 0;
-	TCNT1 = 64286;
-	TCCR1B |= (1<<CS12);
-	TIMSK1 |= (1<<TOIE0);
+	TCCR3A = 0;
+	TCCR3B = 0;
+	TCNT3 = 64286;
+	TCCR3B |= (1<<CS32);
+	TIMSK3 |= (1<<TOIE0);
 }
+
 /*Function to Update commands from ROV*/
-void updateCommands(const arduino_msgs::Commands& cmd_msg){
-	if(cmd_msg.run == 0){
-		thrust.stop();
-	}
-	else{
-		thrust.setValues(cmd_msg.vert,cmd_msg.port_h,cmd_msg.star_h);
-	}
+void updateThruster(const arduino_msgs::Thruster& cmd_msg){
+	thrust.update(cmd_msg.vert,cmd_msg.port_h,cmd_msg.star_h,cmd_msg.run);
 	//Update Cameramount
+	camera.update(cmd_msg.tilt);
+}
+
+void updateCommands(const arduino_msgs::Commands& cmd_msg){
 	//Update Laser setpoint
 	//Update Lighting
+	LED.update(cmd_msg.LED_intensity);
 }
+
 /*Function to update Navigation Message*/
 void updateNavData(){
 	//Update IMU
@@ -47,40 +63,65 @@ void updateNavData(){
 	navdat.gx = gx;
 	navdat.gy = gy;
 	navdat.gz = gz;
-	//Update Temp
 	//Update Depth
-	//Update Humidity
+	navdat.depth=depth_sense.getPressure();
 }
-ISR(TIMER1_OVF_vect){
-	status = 1;
-	TCNT1 = 64286;
+void updateAuxSense()
+{
+	//Update Current
+	//Update Battery Voltage
+	auxdat.batt = ctrl_board.getBatteryVoltage();
+	//Update Temperature
+	auxdat.temperature = ctrl_board.getTemperature();
+	//Update Humidity
+	auxdat.humidity = ctrl_board.getHumidity();
+}
+ISR(TIMER3_OVF_vect){
+	update_nav += 1;
+	update_aux +=1;
+	TCNT3 = 64286;
 }
 ros::Publisher nav_pub("navdat",&navdat);
+ros::Publisher aux_pub("auxdat",&auxdat);
+ros::Subscriber<arduino_msgs::Thruster> thrust_sub("thrust", updateThruster);
 ros::Subscriber<arduino_msgs::Commands> command_sub("commands", updateCommands);
 
 void setup()
 {
+	/*Initialize I2C*/
 	Wire.begin();
+	/*Initialize Sensors*/
 	accelgyro.initialize();
-	thrust.Init();
+	depth_sense.initialize();
+	
+	/*Initialize Thruster and Servo*/
+	thrust.initialize();
+
 	noInterrupts();
-	timer1_init();	
+	timer3_init();	
 	nh.initNode();
 	navdat.header.frame_id = "/navdata";
+	auxdat.header.frame_id = "/auxdat";
 	nh.subscribe(command_sub);
 	nh.advertise(nav_pub);
+	nh.advertise(aux_pub);
 	interrupts();
 }
 
 void loop()
 {
-	if(status == 1){
+	if(update_aux > 4){
+		updateAuxSense();
+		aux_pub.publish(&auxdat);
+		update_aux = 0;
+	}
+	if(update_nav > 0){
 		//Update Navigation message
 		updateNavData();
 		//Publish the navigation data
 		nav_pub.publish(&navdat);
 		nh.spinOnce();
-		status = 0;
+		update_nav = 0;
 	}
 }
 
